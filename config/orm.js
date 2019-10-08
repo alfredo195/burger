3691f1fcd576f51eca35e7f5b92160c165418773
BlackAce <alfredo.gon195@gmail.com>
2019-10-07 21:44:36 -0700

    set up initial directory structure

---
+++ config/orm.js
@@ -0,0 +1,8 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+selectAll()
+insertOne()
+updateOne()
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('inNout');
+INSERT INTO burgers (burger_name) VALUES ('mcdonolds');
+INSERT INTO burgers (burger_name) VALUES ('habbit');
