107e47074206ffbe7f24fda5c24875f92b6ebf2d
BlackAce <alfredo.gon195@gmail.com>
2019-10-09 18:46:03 -0700

    added routes and finished orms

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,16 @@
 // Import MySQL connection.
 var connection = require("../config/connection.js");
 
+function printQuestionMarks(num) {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+}
+
 function objToSql(ob) {
     var arr = [];
   
@@ -33,6 +43,26 @@ function objToSql(ob) {
           }
           cb(result);
         });
+      },
+
+      create: function(table , cols, cb){
+        var queryString = "INSERT INTO " + table;
+        
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString +=" )";
+
+        console/log(queryString);
+
+        connection.query(queryString, vals, function(err, result){
+          if(err) {
+            throw err;
+          }
+          cb(result);
+        })
       },
 
       update: function(table, objColVals, condition, cb) {
